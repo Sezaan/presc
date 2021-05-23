5247413011765b4ab2987b97823f107b915bf4b5
faiazamin <faiazamin.24csedu.040@gmail.com>
2021-05-23 13:03:03 +0600

    new component created

---
+++ src/components/SetNewPassword.js
@@ -0,0 +1,35 @@
+import { TextField } from "@material-ui/core";
+import React, { Component } from "react";
+import style from "../style.module.css";
+
+class SetNewPassword extends Component {
+  state = {};
+  render() {
+    return (
+      <div className={style.loginContainer}>
+        <div className={style.loginBox1}>
+          <span className={style.loginText1}>Set New Password</span>
+          <TextField
+            variant="outlined"
+            label="Set New Password"
+            size="small"
+            style={{ width: "28rem", marginTop: "3.375rem" }}
+          ></TextField>
+          <TextField
+            variant="outlined"
+            label="Retype Password"
+            size="small"
+            style={{ width: "28rem", marginTop: "2.69rem" }}
+          ></TextField>
+          <button className={style.loginButton1}>Set New Password</button>
+          <span className={style.loginText4}>Never Share Your Password With Anyone!</span>
+          
+        </div>
+          
+        
+      </div>
+    );
+  }
+}
+
+export default SetNewPassword;
