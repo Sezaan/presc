86f943b8dae8460ea50b4ba53163546ef64dbdf5
faiazamin <faiazamin.24csedu.040@gmail.com>
2021-06-05 18:12:59 +0600

    general prescription updated

--- src/components/GeneralPrescription.js
+++ src/components/GeneralPrescription.js
@@ -13,218 +13,212 @@ import SaveIcon from "@material-ui/icons/SaveOutlined";
 import SystemUpdateIcon from "@material-ui/icons/SystemUpdateOutlined";
 import FileCopyIcon from "@material-ui/icons/FileCopyOutlined";
 import MyContext from "./MyContext";
-import {Modal} from 'semantic-ui-react'
-
+import { Modal } from "semantic-ui-react";
 
 function GeneralPrescription(props) {
-  
-    const [diagnosisL,setDiagnosis] = useState();
-    const [complaintsL,setComplaint] = useState();
-    const [testsL,setTests] = useState();
-    const [medicinesL,setMedicine] = useState();
-    const [treatmentsL,setTreatment] = useState();
-    const [advicesL,setAdvice] = useState();
-    const {value, setValue} = useContext(MyContext);
-    const [open, setOpen] = useState(false)
+  const [diagnosisL, setDiagnosis] = useState();
+  const [complaintsL, setComplaint] = useState();
+  const [testsL, setTests] = useState();
+  const [medicinesL, setMedicine] = useState();
+  const [treatmentsL, setTreatment] = useState();
+  const [advicesL, setAdvice] = useState();
+  const { value, setValue } = useContext(MyContext);
+  const [open, setOpen] = useState(false);
 
   function saveDiagnosis(values) {
-    setDiagnosis(values)
+    setDiagnosis(values);
+  }
 
-  };
-
-  function saveComplaints (values){
-    setComplaint(values)
-
-  };
+  function saveComplaints(values) {
+    setComplaint(values);
+  }
 
   function saveTests(values) {
-    setTests(values)
-
-  };
+    setTests(values);
+  }
 
   function saveMedicines(values) {
-    setMedicine(values)
-
-  };
+    setMedicine(values);
+  }
 
   function saveTreatments(values) {
-    setTreatment(values)
-  };
+    setTreatment(values);
+  }
 
   function saveAdvices(values) {
-   setAdvice(values)
-  };
+    setAdvice(values);
+  }
 
-  function handlePreview(event){
+  function handlePreview(event) {
     let info = {
       diagnosis: diagnosisL,
       complaints: complaintsL,
       tests: testsL,
       medicines: medicinesL,
       treatments: treatmentsL,
-      advices: advicesL,}
-    ;
+      advices: advicesL,
+    };
     setValue(info);
     setOpen(true);
+  }
 
-
-  };
-
- 
-    return (
+  return (
+    <div
+      style={{
+        borderRadius: "1px",
+        margin: "1rem",
+        background: "#FFFFFF",
+        width: "1350px",
+      }}
+    >
       <div
         style={{
-          borderRadius: "1px",
+          width: "1300px",
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "1rem",
+          marginBottom: "2rem",
+          marginLeft: "1rem",
+        }}
+      >
+        <div>
+          <span style={{ fontWeight: "bold", fontSize: "2rem" }}>
+            Generate Prescription
+          </span>
+        </div>
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <Button
+            variant="outlined"
+            size="medium"
+            style={{
+              margin: "2px",
+              border: "1 solid black",
+              borderRadius: "0px",
+              width: "8rem",
+            }}
+          >
+            Templates
+          </Button>
+          <Button
+            variant="outlined"
+            size="medium"
+            style={{
+              margin: "2px",
+              border: "1 solid black",
+              borderRadius: "0px",
+              width: "8rem",
+            }}
+            startIcon={<SaveIcon />}
+          >
+            Save
+          </Button>
+          <Button
+            variant="outlined"
+            onClickCapture={handlePreview}
+            size="medium"
+            style={{
+              margin: "2px",
+              border: "1 solid black",
+              borderRadius: "0px",
+              width: "8rem",
+            }}
+            startIcon={<DescriptionIcon />}
+          >
+            Preview
+          </Button>
+          <Button
+            variant="outlined"
+            size="medium"
+            style={{
+              margin: "2px",
+              border: "1 solid black",
+              borderRadius: "0px",
+              width: "8rem",
+            }}
+            startIcon={<SystemUpdateIcon />}
+          >
+            Download
+          </Button>
+          <Button
+            variant="outlined"
+            size="medium"
+            style={{
+              margin: "2px",
+              border: "1 solid black",
+              borderRadius: "0px",
+              width: "8rem",
+            }}
+            startIcon={<FileCopyIcon />}
+          >
+            Print
+          </Button>
+        </div>
+        <Modal
+          onOpen={() => setOpen(true)}
+          onClose={() => setOpen(false)}
+          open={open}
+          size="fullscreen"
+        >
+          <Modal.Header>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <p> Prescription&nbsp;Preview</p>
+              <Button onClick={() => setOpen(false)} style={{ color: "red" }}>
+                Close
+              </Button>
+            </div>
+          </Modal.Header>
+          <Modal.Content>
+            <Modal.Description>
+              <Preview />
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
+      </div>
+      <div
+        style={{
+          width: "1300px",
           margin: "1rem",
-          background: "#FFFFFF",
-          width: "1350px",
+          display: "grid",
+          gridTemplateColumns: "1fr 1.5fr 1fr",
+          justifyItems: "start",
+          alignItems: "start",
+          gap: "1.5rem",
         }}
       >
         <div
-          style={{
-            width: "1300px",
-            display: "flex",
-            justifyContent: "space-between",
-            marginTop: "1rem",
-            marginBottom: "2rem",
-            marginLeft: "1rem",
-          }}
-        >
-          <div>
-            <span style={{ fontWeight: "bold", fontSize: "2rem" }}>
-              Generate Prescription
-            </span>
-          </div>
-          <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-            <Button
-              variant="outlined"
-              size="medium"
-              style={{
-                margin: "2px",
-                border: "1 solid black",
-                borderRadius: "0px",
-                width: "8rem",
-              }}
-            >
-              Templates
-            </Button>
-            <Button
-              variant="outlined"
-              size="medium"
-              style={{
-                margin: "2px",
-                border: "1 solid black",
-                borderRadius: "0px",
-                width: "8rem",
-              }}
-              startIcon={<SaveIcon />}
-            >
-              Save
-            </Button>
-            <Button
-              variant="outlined"
-              onClickCapture={handlePreview}
-              size="medium"
-              style={{
-                margin: "2px",
-                border: "1 solid black",
-                borderRadius: "0px",
-                width: "8rem",
-              }}
-              startIcon={<DescriptionIcon />}
-            >
-              Preview
-            </Button>
-            <Button
-              variant="outlined"
-              size="medium"
-              style={{
-                margin: "2px",
-                border: "1 solid black",
-                borderRadius: "0px",
-                width: "8rem",
-              }}
-              startIcon={<SystemUpdateIcon />}
-            >
-              Download
-            </Button>
-            <Button
-              variant="outlined"
-              size="medium"
-              style={{
-                margin: "2px",
-                border: "1 solid black",
-                borderRadius: "0px",
-                width: "8rem",
-              }}
-              startIcon={<FileCopyIcon />}
-            >
-              Print
-            </Button>
-          </div>
-          <Modal
-          onOpen={()=>setOpen(true)}
-          onClose={ ()=> setOpen(false)}
-          open={open}
-          size="fullscreen"
-          >
-            <Modal.Header>
-              <div style={{display:"flex", justifyContent:"space-between"}}>
-              <p> Prescription&nbsp;Preview</p>
-              <Button onClick={ ()=> setOpen(false)} style={{color:"red"}}>Close</Button>
-              </div>
-              
-            </Modal.Header>
-            <Modal.Content>
-              <Modal.Description>
-                <Preview/>
-              </Modal.Description>
-            </Modal.Content>
-          </Modal>
-        </div>
-        <div
-          style={{
-            width: "1300px",
-            margin: "1rem",
-            display: "grid",
-            gridTemplateColumns: "1fr 1.5fr 1fr",
-            gridTemplateRows: "1fr 1fr 1fr fr",
-            justifyItems: "start",
-            alignItems: "start",
-            gap: "1.5rem",
-          }}
+          style={{ display: "flex", flexDirection: "column", flexWrap: "wrap" }}
         >
           <div
-            style={{ gridColumn: "1/5", gridRow: "1/2", width: "fit-content" }}
+            style={{ width: "fit-content", marginBottom:"1rem" }}
           >
             <Diagnosis onChangeDiagnosis={saveDiagnosis}></Diagnosis>
           </div>
-          <div style={{ gridColumn: "1/2", gridRow: "2/3" }}>
+          <div style={{ width: "fit-content", marginBottom:"1rem"  }}>
             <ChiefComplaints
               onChangeComplaints={saveComplaints}
             ></ChiefComplaints>
           </div>
-          <div style={{ gridColumn: "1/2", gridRow: "3/4" }}>
+          <div style={{ width: "fit-content", marginBottom:"1rem" }}>
             <TestAdvice onChangeTests={saveTests}></TestAdvice>
           </div>
-          <div style={{ gridColumn: "1/2", gridRow: "4/5" }}>
+          <div style={{ width: "fit-content", marginBottom:"1rem"  }}>
             <MedicineHistory
               onChangeMedicines={saveMedicines}
             ></MedicineHistory>
           </div>
-          <div
-            style={{ gridColumn: "2/4", gridRow: "1/5", justifySelf: "start" }}
-          >
-            <Treatmeant onChangeTreatments={saveTreatments}></Treatmeant>
-          </div>
-          <div style={{ gridColumn: "4/5", gridRow: "1/5" }}>
-            <Advice onChangeAdvices={saveAdvices}></Advice>
-          </div>
+        </div>
+        <div
+          style={{ width: "fit-content" }}
+        >
+          <Treatmeant onChangeTreatments={saveTreatments}></Treatmeant>
+        </div>
+        <div style={{ width: "fit-content" }}>
+          <Advice onChangeAdvices={saveAdvices}></Advice>
         </div>
       </div>
-    );
-  }
-
+    </div>
+  );
+}
 
 GeneralPrescription.contextType = MyContext;
 
