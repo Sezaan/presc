93f81a09c063e0ef9d0e93ef7b9815684ee07229
faiazamin <faiazamin.24csedu.040@gmail.com>
2021-06-10 19:28:25 +0600

    login signup implemented

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,39 +1,107 @@
 import { TextField } from "@material-ui/core";
-import React, { Component } from "react";
+import React, { Component, useState, } from "react";
+import {useHistory } from "react-router";
 import style from "../style.module.css";
+import {Modal} from 'semantic-ui-react'
 
-class Login extends Component {
-  state = {};
-  render() {
-    return (
-      <div className={style.loginContainer}>
-        <div className={style.loginBox1}>
-          <span className={style.loginText1}>LOGIN</span>
-          <TextField
-            variant="outlined"
-            label="Email Address"
-            size="small"
-            style={{ width: "26rem", marginTop: "3.375rem" }}
-          ></TextField>
-          <TextField
-            variant="outlined"
-            label="Password"
-            type="password"
-            size="small"
-            style={{ width: "26rem", marginTop: "2.69rem" }}
-          ></TextField>
-          <button className={style.loginButton1}>Login</button>
-          <a href="#" className={style.loginText2}>
-            Forgot Password?
-          </a>
-        </div>
-        <div className={style.loginBox2}>
-          <span className={style.loginText3}>Don't Have an Account?</span>
-          <button className={style.loginButton2}>Signup for FREE</button>
-        </div>
-      </div>
-    );
+function Login(props) {
+  const [openModal, setOpenModal] = useState(false);
+  const [responseMsg, setResponseMsg] = useState(null);
+  const history = useHistory();
+
+  // checking for login credential
+  function handleLogin() {
+    // gettng values from textfields
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(email,password)
+
+    // sending post request
+    fetch("https://calm-plains-47385.herokuapp.com/doctor/doctor_signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((responseFromServer) => responseFromServer.json())
+      .then((result) => {
+        // checking if login was successful
+        console.log(result)
+        if (result.token) {
+          console.log(result.token);
+          history.push("/");
+        }
+        else{
+          setResponseMsg(result.msg)
+          setOpenModal(true)
+        }
+      });
   }
+
+  return (
+    <div className={style.loginContainer}>
+      <div className={style.loginBox1}>
+        <span className={style.loginText1}>LOGIN</span>
+        <TextField
+          id="email"
+          variant="outlined"
+          label="Email Address"
+          size="small"
+          style={{ width: "26rem", marginTop: "3.375rem" }}
+        ></TextField>
+        <TextField
+          id="password"
+          variant="outlined"
+          label="Password"
+          type="password"
+          size="small"
+          style={{ width: "26rem", marginTop: "2.69rem" }}
+        ></TextField>
+        <button className={style.loginButton1} onClick={handleLogin}>
+          Login
+        </button>
+        <a href="/forgotpass" className={style.loginText2}>
+          Forgot Password?
+        </a>
+      </div>
+      <Modal
+        closeIcon
+        onOpen={() => setOpenModal(true)}
+        onClose={() => setOpenModal(false)}
+        open={openModal}
+        size="small"
+      >
+        <Modal.Header>There has been an error</Modal.Header>
+        <Modal.Description>
+          <div
+            style={{
+              margin: "auto",
+              fontSize: "20px",
+              fontWeight: "bolder",
+              height: "100px",
+              color: "red",
+              padding: "2rem",
+            }}
+          >
+            {responseMsg}
+          </div>
+        </Modal.Description>
+      </Modal>
+      <div className={style.loginBox2}>
+        <span className={style.loginText3}>Don't Have an Account?</span>
+        <button
+          className={style.loginButton2}
+          onClick={() => history.push("/signup")}
+        >
+          Signup for FREE
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,51 +1,115 @@
 import { TextField } from "@material-ui/core";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import style from "../style.module.css";
+import {Modal} from "semantic-ui-react"
+import {useHistory } from "react-router";
 
-class Signup extends Component {
-  state = {};
-  render() {
-    return (
-      <div className={style.loginContainer}>
-        <div className={style.loginBox1}>
-          <span className={style.loginText1}>Signup</span>
-          <TextField
-            variant="outlined"
-            label="Full Name"
-            size="small"
-            style={{ width: "26rem", marginTop: "3.375rem" }}
-          ></TextField>
-          <TextField
-            variant="outlined"
-            label="Mobile"
-            size="small"
-            style={{ width: "26rem", marginTop: "2.69rem" }}
-          ></TextField>
-          <TextField
-            variant="outlined"
-            label="Email Address"
-            size="small"
-            style={{ width: "26rem", marginTop: "2.69rem" }}
-          ></TextField>
-          <TextField
-            variant="outlined"
-            label="Password"
-            type="password"
-            size="small"
-            style={{ width: "26rem", marginTop: "2.69rem" }}
-          ></TextField>
-          <button className={style.loginButton1}>Create an Account</button>
-          <span  className={style.loginText2}>
+function Signup(props) {
 
-          </span>
-        </div>
-        <div className={style.loginBox2}>
-          <span className={style.loginText3}>Have an Account?</span>
-          <button className={style.loginButton2}>Login</button>
-        </div>
-      </div>
-    );
+  const [openModal, setOpenModal] = useState(false);
+  const [responseMsg, setResponseMsg] = useState(null);
+  const history  = useHistory()
+
+  function handleCreateAccount() {
+    // reading the elements from the text fields
+    const username = document.getElementById("username").value;
+    const mobile = document.getElementById("mobile").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    
+
+    //sending post request to the server
+    //check the post format carefully
+
+    fetch("https://calm-plains-47385.herokuapp.com/doctor/doctor_signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: username,
+        phone: mobile,
+        email: email,
+        password: password,
+      }),
+    })
+      .then((responseFromServer) => responseFromServer.json())
+      .then((result) => {
+    //checking if the post is successful
+        if(result.token){
+          console.log(result.token)
+          history.push("/")
+        }
+        else{
+          setResponseMsg(result.msg)
+          setOpenModal(true)
+        }
+      });
   }
+
+  // redirect to login page
+
+
+  return (
+    <div className={style.loginContainer}>
+      <div className={style.loginBox1}>
+        <span className={style.loginText1}>Signup</span>
+        <TextField
+          id="username"
+          variant="outlined"
+          label="Full Name"
+          size="small"
+          style={{ width: "26rem", marginTop: "3.375rem" }}
+        ></TextField>
+        <TextField
+          id="mobile"
+          variant="outlined"
+          label="Mobile"
+          size="small"
+          style={{ width: "26rem", marginTop: "2.69rem" }}
+        ></TextField>
+        <TextField
+          id="email"
+          variant="outlined"
+          label="Email Address"
+          size="small"
+          style={{ width: "26rem", marginTop: "2.69rem" }}
+        ></TextField>
+        <TextField
+          id="password"
+          variant="outlined"
+          label="Password"
+          type="password"
+          size="small"
+          style={{ width: "26rem", marginTop: "2.69rem" }}
+        ></TextField>
+        <button className={style.loginButton1} onClick={handleCreateAccount}>
+          Create an Account
+        </button>
+        <Modal
+        closeIcon
+        onOpen={()=> setOpenModal(true)}
+        onClose={()=> setOpenModal(false)}
+        open={openModal}
+        size="small"
+        >
+          <Modal.Header>
+            There has been an error
+          </Modal.Header>
+          <Modal.Description>
+            <div style={{margin:"auto",fontSize:"20px",fontWeight:"bolder",height:"100px",color:"red",padding:"2rem"}}>
+                {responseMsg}
+            </div>
+          </Modal.Description>
+        </Modal>
+        <span className={style.loginText2}></span>
+      </div>
+      <div className={style.loginBox2}>
+        <span className={style.loginText3}>Have an Account?</span>
+        <button className={style.loginButton2} onClick={() => history.push('/login')}>Login</button>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
