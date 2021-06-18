0bf647a94d67c46c7cb2d5b8068ddc6c365d32f3
faiazamin <faiazamin.24csedu.040@gmail.com>
2021-06-18 10:26:54 +0600

    header and footer update enabled

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,52 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { Button } from "@material-ui/core";
 import style from "../style.module.css";
+import { useHistory, useParams } from "react-router-dom";
 
 function Profile(pros) {
+  const [profileInfo, setProfileInfo] = useState({});
+  const [header, setHeader] = useState({ });
+  const [degree, setDegree] = useState([]);
+  const [lines, setLines] = useState([]);
+  const history = useHistory();
+  // getting profile information
+  useEffect(() => {
+    fetch("https://calm-plains-47385.herokuapp.com/doctor", {
+      headers: {
+        "prescription-generator": `${localStorage.getItem("auth_token")}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((respone) => respone.json())
+      .then((result) => {
+
+        
+        setProfileInfo(result);
+
+        setHeader(result.header);
+        if(result.header.degrees)
+        setDegree(result.header.degrees);
+        if(result.header.lines)
+        setLines(result.header.lines);
+
+        localStorage.setItem("email", result.email);
+      });
+  });
+
+  //sending verify email
+  function handleVerify() {
+    fetch("https://calm-plains-47385.herokuapp.com/doctor/verification_email", {
+      headers: {
+        "prescription-generator": `${localStorage.getItem("auth_token")}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((respone) => respone.json())
+      .then((result) => console.log(result));
+
+    console.log(localStorage.getItem("auth_token"));
+  }
+
   return (
     <div
       style={{
@@ -13,191 +57,246 @@ function Profile(pros) {
         marginBottom: "1rem",
       }}
     >
-      <p style={{fontWeight:"bold",fontSize:"26px"}}> Profile</p>
+      <p style={{ fontWeight: "bold", fontSize: "26px" }}>Profile</p>
       <br />
-      <span style={{fontWeight:"bold",fontSize:"26px"}}>Hi, Dr. Rajesh Kuthrapali </span>
-      <Button style={{marginLeft:"2rem",borderRadius:"20px", border:"1px solid red", backgroundColor:"#F9EAEA",color:"red"}}>verify email</Button>
+      <span style={{ fontWeight: "bold", fontSize: "26px" }}>
+        {profileInfo.name}
+      </span>
+      {profileInfo.verified ? (
+        <Button
+          disabled
+          style={{
+            marginLeft: "2rem",
+            borderRadius: "20px",
+            border: "1px solid green",
+            backgroundColor: "#8FDE1F",
+            color: "green",
+          }}
+          onClick={handleVerify}
+        >
+          verified
+        </Button>
+      ) : (
+        <Button
+          style={{
+            marginLeft: "2rem",
+            borderRadius: "20px",
+            border: "1px solid red",
+            backgroundColor: "#F9EAEA",
+            color: "red",
+          }}
+          onClick={handleVerify}
+        >
+          verify email
+        </Button>
+      )}
+
       <br />
       <br />
       <br />
       <br />
       <br />
-      <span style={{fontWeight:"bold",fontSize:"16px"}}>Basic Information</span>
-      <Button variant="outlined" style={{marginLeft:"3.5rem"}}>Edit Profile</Button>
+      <span style={{ fontWeight: "bold", fontSize: "16px" }}>
+        Basic Information
+      </span>
+      <Button
+        variant="outlined"
+        style={{ marginLeft: "3.5rem" }}
+        onClick={() => {
+          history.push("./edit_profile");
+        }}
+      >
+        Edit Profile
+      </Button>
       <hr />
       <br />
 
       <div
         style={{
           display: "grid",
-          gridTemplateColumns:"repeat(auto-fit,minmax(400px,1fr))",
+          gridTemplateColumns: "repeat(auto-fit,minmax(400px,1fr))",
           justifyContent: "flex-start",
         }}
       >
-        <table style={{ textAlign: "start", width:"800px" }}>
-          <tr>
-            <td>
-              <span>Full Name</span>
-            </td>
-            <td>
-              <span>Rajesh Kuthrapali</span>
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span>Email Addrss</span>
-            </td>
-            <td>
-              <span>contact@gmail.com</span>
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span>Phone Number</span>
-            </td>
-            <td>
-              <span>01535678988</span>
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span>Subscription Type</span>
-            </td>
-            <td>
-              <span>free trial</span>
-              <Button style={{marginLeft:"2rem",borderRadius:"20px", border:"1px solid red", backgroundColor:"#E55050",color:"white"}}>Upgrad to primium</Button>
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span> Expriry On</span>
-            </td>
-            <td>
-              <span>30th June,2021</span>
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span>Prescription Limit</span>
-            </td>
-            <td>
-              <span>977/1000 used</span>
-            </td>
-          </tr>
+        <table style={{ textAlign: "start", width: "800px" }}>
+          <tbody>
+            <tr>
+              <td>
+                <span>Full Name</span>
+              </td>
+              <td>
+                <span>{profileInfo.name}</span>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span>Email Address</span>
+              </td>
+              <td>
+                <span>{profileInfo.email}</span>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span>Phone Number</span>
+              </td>
+              <td>
+                <span>+88{profileInfo.phone}</span>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span>Subscription Type</span>
+              </td>
+              <td>
+                {profileInfo.subscribed ? (
+                  <span>Premium</span>
+                ) : (
+                  <span>Free Trial</span>
+                )}
+                <Button
+                  style={{
+                    marginLeft: "2rem",
+                    borderRadius: "20px",
+                    border: "1px solid red",
+                    backgroundColor: "#E55050",
+                    color: "white",
+                  }}
+                >
+                  Upgrad to primium
+                </Button>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span> Expriry On</span>
+              </td>
+              <td>
+                <span>{profileInfo.expire_at}</span>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span>Prescription Limit</span>
+              </td>
+              <td>
+                <span>{profileInfo.prescription_limit}</span>
+              </td>
+            </tr>
+          </tbody>
         </table>
         <div></div>
       </div>
       <br />
-      <span style={{fontWeight:"bold",fontSize:"16px"}}>Prescription Header</span>
-      <Button variant="outlined" style={{marginLeft:"2rem"}}>Edit Page Setup</Button>
+      <span style={{ fontWeight: "bold", fontSize: "16px" }}>
+        Prescription Header
+      </span>
+      <Button onClick={() => history.push('/editheader')} variant="outlined" style={{ marginLeft: "2rem" }}>
+        Edit Page Setup
+      </Button>
       <hr />
       <br />
 
       <div
         style={{
           display: "grid",
-          gridTemplateColumns:"repeat(auto-fit,minmax(400px,1fr))",
+          gridTemplateColumns: "repeat(auto-fit,minmax(400px,1fr))",
           justifyContent: "flex-start",
         }}
       >
-        <table style={{ textAlign: "start", width:"690px"}}>
-          <tr>
-            <td>
-              <span>Prescription Name</span>
-            </td>
-            <td>
-              <span>Rajesh Kuthrapali</span>
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span>Degree</span>
-            </td>
-            <td>
-              <span>Degree Name</span>
-              <br />
-              <span>Degree Name</span>
-              <br />
-              <span>Degree Name</span>
-              <br />
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span>Hospital/Workplace</span>
-            </td>
-            <td>
-              <span>Dhaka Medical College Hospital</span>
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span>Chamber Address</span>
-            </td>
-            <td>
-              <span>My Addreess</span>
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span>Chamber Phone</span>
-            </td>
-            <td>
-              <span>+88012212323</span>
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span>Chamber Email</span>
-            </td>
-            <td>
-              <span>contact@gamil.com</span>
-            </td>
-          </tr>
-          <br/>
-          <tr>
-            <td>
-              <span>Extra Lines</span>
-            </td>
-            <td>
-              <span>some text here</span>
-              <br />
-              <span>some text here</span>
-            </td>
-          </tr>
+        <table style={{ textAlign: "start", width: "690px" }}>
+          <tbody>
+            <tr>
+              <td>
+                <span>Prescription Name</span>
+              </td>
+              <td>
+                <span>{header.name}</span>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span>Degree</span>
+              </td>
+              <td>{degree.map(item => <p>{item.value}</p>)}</td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span>Hospital/Workplace</span>
+              </td>
+              <td>
+                <span>{header.work}</span>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span>Chamber Address</span>
+              </td>
+              <td>
+                <span>{header.address}</span>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span>Chamber Phone</span>
+              </td>
+              <td>
+                <span>{header.phone}</span>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span>Chamber Email</span>
+              </td>
+              <td>
+                <span>{header.email}</span>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <span>Extra Lines</span>
+              </td>
+              <td>{lines.map(item => <p>{item.value}</p>)}</td>
+            </tr>
+          </tbody>
         </table>
         <div></div>
       </div>
       <br />
-      <span style={{fontWeight:"bold",fontSize:"16px"}}>Prescription Footer</span>
+      <span style={{ fontWeight: "bold", fontSize: "16px" }}>
+        Prescription Footer
+      </span>
       <hr />
       <br />
       <div
         style={{
           display: "grid",
-          gridTemplateColumns:"repeat(auto-fit,minmax(400px,1fr))",
+          gridTemplateColumns: "repeat(auto-fit,minmax(400px,1fr))",
           justifyContent: "flex-start",
         }}
       >
-        <table style={{ textAlign: "start", width:"800px" }}>
-          <tr>
-            <td>
-              <span> Footer Text</span>
-            </td>
-            <td>
-              <span>One line text for footer </span>
-            </td>
-          </tr>
+        <table style={{ textAlign: "start", width: "800px" }}>
+          <tbody>
+            <tr>
+              <td>
+                <span> Footer Text</span>
+              </td>
+              <td>
+                <span>{profileInfo.footer}</span>
+              </td>
+            </tr>
+          </tbody>
         </table>
         <div></div>
       </div>
--- src/components/Setup.js
+++ src/components/Setup.js
@@ -1,9 +1,160 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { Button, TextField } from "@material-ui/core";
-import AddIcon from '@material-ui/icons/Add';
+import AddIcon from "@material-ui/icons/Add";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import ListItem from "./ListItem";
+import { Item } from "semantic-ui-react";
 
 function Setup(props) {
+  const [header, setHeader] = useState([]);
+  const [degrees, setDegrees] = useState([]);
+  const [degreeCounter, setDegreeCounter] = useState(0);
+  const [lines, setLines] = useState([]);
+  const [lineCounter, setLineCounter] = useState(0);
+
+  // fetch information from server at the time start
+  useEffect(() => {
+    fetch("https://calm-plains-47385.herokuapp.com/doctor", {
+      headers: {
+        "prescription-generator": `${localStorage.getItem("auth_token")}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (!result.success) {
+          setHeader(result);
+        }
+      });
+  }, false);
+
+  // send update header request
+  function updateHeader() {
+    const name = document.getElementById("name");
+    const work = document.getElementById("work");
+    const email = document.getElementById("email");
+    const address = document.getElementById("address");
+    const phone = document.getElementById("phone");
+    let headerInput = {};
+
+    if (name.value !== "") {
+      headerInput["name"] = name.value;
+    }
+    if (work.value !== "") {
+      headerInput["work"] = work.value;
+    }
+    if (email.value !== "") {
+      headerInput["email"] = email.value;
+    }
+    if (address.value !== "") {
+      headerInput["address"] = address.value;
+    }
+    if (phone.value !== "") {
+      headerInput["phone"] = phone.value;
+    }
+
+    headerInput["degrees"] = degrees;
+
+    headerInput["lines"] = lines;
+
+    console.log("this is header input",headerInput);
+
+    fetch("https://calm-plains-47385.herokuapp.com/doctor/edit_profile", {
+      method: "PUT",
+      headers: {
+        "prescription-generator": `${localStorage.getItem("auth_token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: header.name,
+        phone: header.phone,
+        header: headerInput,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      });
+  }
+
+  // send update footer request to server
+  function updateFooter() {
+    const footer = document.getElementById("footer");
+    console.log(footer.value);
+    if (footer.value) {
+      fetch("https://calm-plains-47385.herokuapp.com/doctor/edit_profile", {
+        method: "PUT",
+        headers: {
+          "prescription-generator": `${localStorage.getItem("auth_token")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: header.name,
+          phone: header.phone,
+          footer: footer.value,
+        }),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result);
+        });
+    }
+  }
+
+  //add new degree
+  function handleAddIconForDegree() {
+    const degree = document.getElementById("degree");
+    if (degree.value !== "") {
+      let values = degrees;
+      values.push({ id: degreeCounter, value: degree.value });
+      setDegrees(values);
+      setDegreeCounter(degreeCounter + 1);
+      degree.value = "";
+    }
+  }
+
+  //add new degree on Enter
+
+  function addDegreeOnEnter(event) {
+    console.log(event);
+    if (event.code === "Enter") {
+      handleAddIconForDegree();
+    }
+  }
+
+  //add new line
+  function handleAddIconForLine() {
+    const extra = document.getElementById("extra");
+    if (extra.value !== "") {
+      let values = lines;
+      values.push({ id: lineCounter, value: extra.value });
+      setLines(values);
+      setLineCounter(lineCounter + 1);
+      extra.value = "";
+    }
+  }
+
+  //add new line on Enter
+
+  function addLineOnEnter(event) {
+    console.log(event);
+    if (event.code === "Enter") {
+      handleAddIconForLine();
+    }
+  }
+
+  //remove degree
+  function handleCrossDegrees(id) {
+    let values = degrees.filter((item) => item.id !== id);
+    setDegrees(values);
+  }
+
+  //remove Llnes
+  function handleCrossLines(id) {
+    let values = lines.filter((item) => item.id !== id);
+    setLines(values);
+  }
+
   return (
     <div
       style={{
@@ -61,6 +212,7 @@ function Setup(props) {
               background: "#e55050",
               border: "0px",
             }}
+            onClick={updateHeader}
           >
             Save&nbsp;Header
           </button>
@@ -82,19 +234,40 @@ function Setup(props) {
             rowGap: "1rem",
           }}
         >
-          <TextField variant="outlined" label="Display Name"></TextField>
           <TextField
+            id="name"
+            variant="outlined"
+            label="Display Name"
+          ></TextField>
+          {degrees.map((item) => (
+            <ListItem
+              key={item.id}
+              id={item.id}
+              item={item.value}
+              onCrossClicked={handleCrossDegrees}
+            />
+          ))}
+          <TextField
+            id="degree"
             variant="outlined"
             label="Degree"
+            onKeyDown={addDegreeOnEnter}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="start">
-                  <AddIcon style={{ cursor: "pointer" }} />
+                  <AddIcon
+                    style={{ cursor: "pointer" }}
+                    onClick={handleAddIconForDegree}
+                  />
                 </InputAdornment>
               ),
             }}
           ></TextField>
-          <TextField variant="outlined" label="Hospital/Work"></TextField>
+          <TextField
+            id="work"
+            variant="outlined"
+            label="Hospital/Work"
+          ></TextField>
         </div>
         <div
           style={{
@@ -105,18 +278,40 @@ function Setup(props) {
           }}
         >
           <TextField
+            id="address"
             variant="outlined"
             label="Chamber&nbsp;Address"
           ></TextField>
-          <TextField variant="outlined" label="Chamber&nbsp;Phone"></TextField>
-          <TextField variant="outlined" label="Chamber&nbsp;Email"></TextField>
           <TextField
+            id="phone"
+            variant="outlined"
+            label="Chamber&nbsp;Phone"
+          ></TextField>
+          <TextField
+            id="email"
+            variant="outlined"
+            label="Chamber&nbsp;Email"
+          ></TextField>
+          {lines.map((item) => (
+            <ListItem
+              key={item.id}
+              id={item.id}
+              item={item.value}
+              onCrossClicked={handleCrossLines}
+            />
+          ))}
+          <TextField
+            id="extra"
             variant="outlined"
             label="Extra Lines"
+            onKeyDown={addLineOnEnter}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="start">
-                  <AddIcon style={{ cursor: "pointer" }} />
+                  <AddIcon
+                    style={{ cursor: "pointer" }}
+                    onClick={handleAddIconForLine}
+                  />
                 </InputAdornment>
               ),
             }}
@@ -155,6 +350,7 @@ function Setup(props) {
               background: "#e55050",
               border: "0px",
             }}
+            onClick={updateFooter}
           >
             Save&nbsp;Footer
           </button>
@@ -166,7 +362,11 @@ function Setup(props) {
           gridTemplateColumns: "repeat(suto-fit,minmax(500px,1fr))",
         }}
       >
-        <TextField variant="outlined" label="Footer Text"></TextField>
+        <TextField
+          id="footer"
+          variant="outlined"
+          label="Footer Text"
+        ></TextField>
       </div>
     </div>
   );
